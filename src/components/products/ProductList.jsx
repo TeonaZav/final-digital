21381fda81c2dd182a033ab3e2087dae63ab126e
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-11-30 18:21:09 +0400

    feat: add ProductList component

---
+++ src/components/products/ProductList.jsx
@@ -0,0 +1,13 @@
+import { ProductCard } from "./../";
+
+const ProductList = ({ products }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;

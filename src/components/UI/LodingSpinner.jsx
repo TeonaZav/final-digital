ce7793fae1667b997529f489409cf47f0c86a42d
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-11-30 13:33:42 +0400

    feat: update LodingSpinner component

--- src/components/UI/LodingSpinner.jsx
+++ src/components/UI/LodingSpinner.jsx
@@ -2,7 +2,7 @@ import { Spinner } from "@material-tailwind/react";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-end gap-8">
+    <div className="w-full h-full flex items-start justify-center gap-8 pt-40">
       <Spinner className="h-12 w-12" />
     </div>
   );

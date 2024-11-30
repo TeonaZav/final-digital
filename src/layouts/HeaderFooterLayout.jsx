58b4ab11c100f8ad73bb0c08dd9710feeb904682
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-11-30 23:09:01 +0400

    refactor: update layout

--- src/layouts/HeaderFooterLayout.jsx
+++ src/layouts/HeaderFooterLayout.jsx
@@ -3,9 +3,9 @@ import { Header, Footer } from "./";
 
 const HeaderFooterLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <Header />
-      <main className="py-20">
+      <main className="py-20 flex-1">
         <Outlet />
       </main>
       <Footer />

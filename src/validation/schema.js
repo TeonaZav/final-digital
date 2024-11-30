7dba21ea1ed8fc34ded132d9349914a51a04fced
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-11-30 22:19:08 +0400

    feat: add validation schemas for login and register

---
+++ src/validation/schema.js
@@ -0,0 +1,29 @@
+import * as yup from "yup";
+
+export const registrationSchema = yup.object().shape({
+  first_name: yup.string().required("First name is required"),
+  last_name: yup.string().required("Last name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+  phone_number: yup
+    .string()
+    .matches(
+      /^[0-9]{9,14}$/,
+      "Phone number must be between 9 and 14 digits and numeric only"
+    )
+    .required("Phone number is required"),
+});
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});

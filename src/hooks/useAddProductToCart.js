e15d9605f9063a920e737de8fb20d693bb327470
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-12-01 06:31:22 +0400

    update useAddProductToCart and useUserLogin custom hooks

--- src/hooks/useAddProductToCart.js
+++ src/hooks/useAddProductToCart.js
@@ -1,24 +1,3 @@
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
-// import { addProductToCart } from "../services/api";
-// import { toast } from "react-toastify";
-
-// export const useAddProductToCart = (accessToken) => {
-//   const queryClient = useQueryClient();
-
-//   const { mutate: addProduct, isLoading } = useMutation({
-//     mutationFn: (id) => addProductToCart(id, accessToken),
-//     onSuccess: () => {
-//       toast.success("Product added to cart");
-//       queryClient.invalidateQueries(["cart"]);
-//     },
-//     onError: (err) => {
-//       toast.error(err.message || "Failed to add product to cart.");
-//     },
-//   });
-
-//   return { addProduct, isLoading };
-// };
-
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useDispatch } from "react-redux";
 import { addProductToCart } from "../services/api";
@@ -31,10 +10,8 @@ export const useAddProductToCart = (accessToken) => {
   const { mutate: addProduct, isLoading } = useMutation({
     mutationFn: (product) => addProductToCart(product, accessToken),
     onSuccess: (product) => {
-      // Update Redux cart state
       dispatch(addItem(product));
 
-      // Invalidate cart query
       queryClient.invalidateQueries(["cart"]);
 
       toast.success("Product added to cart");

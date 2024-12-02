61120fbcc9f18d82331b514b2d07110dbf9cd506
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-12-02 23:58:35 +0400

    feat: create checkout page

--- src/pages/user/Checkout.jsx
+++ src/pages/user/Checkout.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  CheckoutProductList,
+  CheckoutTotals,
+  Heading,
+  PaymentOptions,
+} from "../../components";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const [selectedPayment, setSelectedPayment] = useState("");
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+
+  const handlePaymentChange = (paymentId) => {
+    setSelectedPayment(paymentId);
+  };
+  return (
+    <div className="grid grid-cols-12 gap-6 w-full p-4 ">
+      <div className="col-span-8 space-y-4">
+        <CheckoutProductList cartItems={cartItems} />
+        <PaymentOptions
+          selectedPayment={selectedPayment}
+          onSelectOption={handlePaymentChange}
+        />
+      </div>
+
+      <div className="col-span-4">
+        <CheckoutTotals
+          selectedPayment={selectedPayment}
+          cartItems={cartItems}
+        />
+      </div>
+    </div>
+  );
 };
+
 export default Checkout;

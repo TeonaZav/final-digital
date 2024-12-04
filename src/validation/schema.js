5fe45eb482ffb0073cf52390787b4e65e2868018
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-12-05 02:48:28 +0400

    feat: add validation schemas for category and product forms

--- src/validation/schema.js
+++ src/validation/schema.js
@@ -28,3 +28,62 @@ export const loginSchema = yup.object().shape({
     .required("ელ. ფოსტა სავალდებულოა"),
   password: yup.string().required("პაროლი სავალდებულოა"),
 });
+
+export const productValidationSchema = yup.object().shape({
+  image: yup
+    .mixed()
+    .required("სავალდებულო")
+    .test("fileFormat", "დასაშვებია მხოლოდ სურათის ტიპი", (value) => {
+      return (
+        value &&
+        (typeof value === "string" ||
+          [
+            "image/jpeg",
+            "image/png",
+            "image/gif",
+            "image/svg+xml",
+            "image/webp",
+          ].includes(value.type))
+      );
+    }),
+  title: yup.string().required("სავალდებულო"),
+  description: yup.string().required("სავალდებულო"),
+  price: yup
+    .number()
+    .nullable()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    )
+    .required("სავალდებულო")
+    .typeError("მხოლოდ რიცხვები"),
+
+  salePrice: yup
+    .number()
+    .nullable()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    )
+    .typeError("მხოლოდ რიცხვები"),
+
+  category_name: yup.string().required("სავალდებულო"),
+});
+
+export const categoryValidationSchema = yup.object().shape({
+  image: yup
+    .mixed()
+    .required("სავალდებულო")
+    .test("fileFormat", "დასაშვებია მხოლოდ სურათის ტიპი", (value) => {
+      return (
+        value &&
+        (typeof value === "string" ||
+          [
+            "image/jpeg",
+            "image/png",
+            "image/gif",
+            "image/svg+xml",
+            "image/webp",
+          ].includes(value.type))
+      );
+    }),
+  name: yup.string().required("სავალდებულო"),
+});
